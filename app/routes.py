1158aaaee7dd89924321e7b776ae0831d1a94639
Rayhan Rahman <rayhan_rahman@hotmail.com>
2019-12-08 03:13:04 -0800

    completed Chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'rayhan'}
+    posts = [
+        {
+            'author': {'username': 'rayhan'},
+            'body': 'Beautiful day in Victoria!'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
